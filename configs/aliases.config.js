33dfd75b90d48804cf6f548942ec6ba6ba6f0337
cahcnc91 <camilanierocosta@hotmail.com>
2020-05-08 14:54:03 -0400

    test

--- configs/aliases.config.js
+++ configs/aliases.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 // __dirname gives exact path where the file is, in this case: PROJECT/configs. It is needed to add ../ at the beginning of the aliases to solve correct path.
 
 const aliases = {
-    react: path.resolve('../node_modules/react'),
+    //react: path.resolve('../node_modules/react'),
     'cahcnc91-autocomplete': path.resolve(__dirname, '../src/Autocomplete/src/index'),
     'cahcnc91-click-scroll': path.resolve(__dirname, '../src/ClickAndScroll/src/index'),
 };
